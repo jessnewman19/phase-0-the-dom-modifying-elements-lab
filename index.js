77f5182a677d6142dc011a49ab80f272d3856653
Jessica <jess.newman19@gmail.com>
2022-04-08 11:46:32 -0400

    commit tree cfbe97af2f0d55be028a411fc8d8ada9355f9500

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+//remove DOM node Main
+
+const element = document.getElementById("main"); 
+element.remove()
+
+// has a 'newHeader' variable that points to 
+// node 'h1#victory'"
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+//has a 'newHeader' variable that points to node 
+// 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.textContent = "YOUR-NAME is the champion"
+
+document.body.append(newHeader)
+
+
